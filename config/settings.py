3bd18b8c325eaa8eb98a124e917aaf8e9570dd3a
matsnevIn <v.matsnev@hotmail.com>
2022-08-31 10:55:36 +0000

    lesson_4 Models

--- config/settings.py
+++ config/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
