c17bbc82e8a04100c41ec661a6883ba1f23fb45b
matsnevIn <v.matsnev@hotmail.com>
2022-09-02 12:30:50 +0000

    lesson_5 Login page

--- authapp/views.py
+++ authapp/views.py
@@ -1,2 +1,31 @@
+from django.contrib import messages
+from django.contrib.auth.views import LoginView, LogoutView
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy as _
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    def form_valid(self, form):
+        ret = super().form_valid(form)
+        message = _("Login success!<br>Hi, %(username)s") % {
+            "username": self.request.user.get_full_name()
+            if self.request.user.get_full_name()
+            else self.request.user.get_username()
+        }
+        messages.add_message(self.request, messages.INFO, mark_safe(message))
+        return ret
+
+    def form_invalid(self, form):
+        for _unused, msg in form.error_messages.items():
+            messages.add_message(
+                self.request,
+                messages.WARNING,
+                mark_safe(f"Something goes worng:<br>{msg}"),
+            )
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+class CustomLogoutView(LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        messages.add_message(self.request, messages.INFO, _("See you later!"))
+        return super().dispatch(request, *args, **kwargs)
