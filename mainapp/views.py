d00e98f7c1f80c49e60a0342ea5741a195a81441
matsnevIn <v.matsnev@hotmail.com>
2022-09-02 12:52:41 +0000

    lesson_5 Authapp done

--- mainapp/views.py
+++ mainapp/views.py
@@ -57,7 +57,3 @@ class ContactsPageView(TemplateView):
 
 class DocSitePageView(TemplateView):
     template_name = "mainapp/doc_site.html"
-
-
-class LoginPageView(TemplateView):
-    template_name = "mainapp/login.html"
