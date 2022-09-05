1ca9994ce2396aa320b91d60f04c6f542b1cd7bb
matsnevIn <v.matsnev@hotmail.com>
2022-09-05 10:54:28 +0300

    Lesson 5 (#4)
    * lesson_5

---
+++ authapp/views.py
@@ -0,0 +1,73 @@
+from django.contrib import messages
+from django.contrib.auth.views import LoginView, LogoutView
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import TemplateView
+
+from authapp import models
+
+
+class CustomLoginView(LoginView):
+    def form_valid(self, form):
+        ret = super().form_valid(form)
+        message = _("Login success!<br>Hi, %(username)s") % {
+            "username": self.request.user.get_full_name()
+            if self.request.user.get_full_name()
+            else self.request.user.get_username()
+        }
+        messages.add_message(self.request, messages.INFO, mark_safe(message))
+        return ret
+
+    def form_invalid(self, form):
+        for _unused, msg in form.error_messages.items():
+            messages.add_message(
+                self.request,
+                messages.WARNING,
+                mark_safe(f"Something goes worng:<br>{msg}"),
+            )
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+class CustomLogoutView(LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        messages.add_message(self.request, messages.INFO, _("See you later!"))
+        return super().dispatch(request, *args, **kwargs)
+
+
+class RegisterView(TemplateView):
+    template_name = "registration/register.html"
+
+    def post(self, request, *args, **kwargs):
+        try:
+            if all(
+                (
+                    request.POST.get("username"),
+                    request.POST.get("email"),
+                    request.POST.get("password1"),
+                    request.POST.get(
+                        "password1") == request.POST.get("password2"),
+                )
+            ):
+                new_user = models.CustomUser.objects.create(
+                    username=request.POST.get("username"),
+                    first_name=request.POST.get("first_name"),
+                    last_name=request.POST.get("last_name"),
+                    age=request.POST.get(
+                        "age") if request.POST.get("age") else 0,
+                    avatar=request.FILES.get("avatar"),
+                    email=request.POST.get("email"),
+                )
+                new_user.set_password(request.POST.get("password1"))
+                new_user.save()
+                messages.add_message(request, messages.INFO,
+                                     _("Registration success!"))
+                return HttpResponseRedirect(reverse_lazy("authapp:login"))
+        except Exception as exp:
+            messages.add_message(
+                request,
+                messages.WARNING,
+                mark_safe(f"Something goes worng:<br>{exp}"),
+            )
+            return HttpResponseRedirect(reverse_lazy("authapp:register"))
--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "markdownify.apps.MarkdownifyConfig",
+    "social_django",
     "mainapp",
+    "authapp",
 ]
 
 MIDDLEWARE = [
@@ -64,9 +66,12 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+                "django.template.context_processors.media",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "mainapp.context_processors.example.simple_context_processor"
+                "mainapp.context_processors.example.simple_context_processor",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
             ],
         },
     },
@@ -104,6 +109,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "authapp.CustomUser"
+
+AUTHENTICATION_BACKENDS = (
+    "social_core.backends.github.GithubOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
+)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -132,3 +144,27 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Media files
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_REDIRECT_URL = "mainapp:main_page"
+LOGOUT_REDIRECT_URL = "mainapp:main_page"
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+
+SOCIAL_AUTH_GITHUB_KEY = "8af1701bc66cc5f81c6a"
+SOCIAL_AUTH_GITHUB_SECRET = "70abd82951c0051a27d532d6e6e0c8814e4ee451"
+
+
+
+
+
+
+
+
+
+
