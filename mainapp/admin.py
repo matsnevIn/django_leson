922ea5101b72d31a1794164470211e0902a9cfe8
matsnevIn <v.matsnev@hotmail.com>
2022-09-06 10:42:52 +0000

    lesson_6 Link to admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,1 +1,9 @@
 # Register your models here.
+from django.contrib import admin
+
+from mainapp import models as mainapp_models
+
+
+@admin.register(mainapp_models.News)
+class NewsAdmin(admin.ModelAdmin):
+    pass
