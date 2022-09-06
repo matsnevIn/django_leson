7b31086573ba90227e6065f68151b86331112091
matsnevIn <v.matsnev@hotmail.com>
2022-09-06 10:56:33 +0000

    lesson_6 Link to admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -7,3 +7,4 @@ from mainapp import models as mainapp_models
 @admin.register(mainapp_models.News)
 class NewsAdmin(admin.ModelAdmin):
     pass
+
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class News(models.Model):
     title = models.CharField(max_length=256, verbose_name="Title")
     preambule = models.CharField(max_length=1024, verbose_name="Preambule")
     body = models.TextField(blank=True, null=True, verbose_name="Body")
-    body_as_markdown = models.BooleanField(default=False, verbose_name="As markdown")
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
-    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
+    body_as_markdown = models.BooleanField(
+        default=False, verbose_name="As markdown")
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(
+        auto_now=True, verbose_name="Edited", editable=False)
     deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
@@ -16,6 +20,11 @@ class News(models.Model):
     def delete(self, *args):
         self.deleted = True
         self.save()
+
+    class Meta:
+        verbose_name = _("News")
+        verbose_name_plural = _("News")
+        ordering = ("-created",)
 
 
 class CoursesManager(models.Manager):
@@ -27,10 +36,14 @@ class Courses(models.Model):
     objects = CoursesManager()
 
     name = models.CharField(max_length=256, verbose_name="Name")
-    description = models.TextField(verbose_name="Description", blank=True, null=True)
-    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
-    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
-    cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
+    description = models.TextField(
+        verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(
+        verbose_name="As markdown", default=False)
+    cost = models.DecimalField(
+        max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
+    cover = models.CharField(
+        max_length=25, default="no_image.svg", verbose_name="Cover")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
     updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
     deleted = models.BooleanField(default=False)
@@ -47,10 +60,14 @@ class Lesson(models.Model):
     course = models.ForeignKey(Courses, on_delete=models.CASCADE)
     num = models.PositiveIntegerField(verbose_name="Lesson number")
     title = models.CharField(max_length=256, verbose_name="Name")
-    description = models.TextField(verbose_name="Description", blank=True, null=True)
-    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
-    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
+    description = models.TextField(
+        verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(
+        verbose_name="As markdown", default=False)
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(
+        auto_now=True, verbose_name="Edited", editable=False)
     deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
@@ -62,6 +79,8 @@ class Lesson(models.Model):
 
     class Meta:
         ordering = ("course", "num")
+        verbose_name = _("Lesson")
+        verbose_name_plural = _("Lessons")
 
 
 class CourseTeachers(models.Model):
@@ -77,3 +96,7 @@ class CourseTeachers(models.Model):
     def delete(self, *args):
         self.deleted = True
         self.save()
+
+    class Meta:
+        verbose_name = _("Teacher")
+        verbose_name_plural = _("Teachers")
